53a1d4ea79a163f988b6a44193fe5eb9e93adb55
jack lus <91653828+qdbihc@users.noreply.github.com>
2026-03-28 13:32:41 +0800

    Update ControlWidget.swift

--- EasyTierWidgetExtension/ControlWidget.swift
+++ EasyTierWidgetExtension/ControlWidget.swift
@@ -1,21 +1,17 @@
-#if os(iOS) && canImport(WidgetKit)
-import WidgetKit
+#if os(iOS)
 import SwiftUI
-import Intents
+import WidgetKit
 
 @available(iOS 18.0, *)
 struct EasyTierControlWidget: ControlWidget {
-    static let kind: String = "EasyTierControlWidget"
-
+    static let kind: String = "EasyTierControl"
+    
     var body: some ControlWidgetConfiguration {
         ControlWidgetConfiguration {
             ControlWidgetButton(action: {}) {
                 Label("EasyTier", systemImage: "network")
             }
         }
-        .configurationDisplayName("EasyTier Control")
-        .description("Quick control widget")
     }
 }
-
 #endif
