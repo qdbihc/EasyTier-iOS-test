ce38a8e4bd876007fcb204188912155cbdfc5ae1
jack lus <91653828+qdbihc@users.noreply.github.com>
2026-03-28 13:07:44 +0800

    Update ControlWidget.swift

--- EasyTierWidgetExtension/ControlWidget.swift
+++ EasyTierWidgetExtension/ControlWidget.swift
@@ -3,40 +3,33 @@ import WidgetKit
 import SwiftUI
 import Intents
 
-// 仅在 iOS 18.0 及以上版本生效
-// iOS 16 / 17 会自动忽略，不会编译，不会报错
+// 仅在 iOS 18.0+ 编译和注册，iOS 16/17 完全忽略
 @available(iOS 18.0, *)
 struct EasyTierControlWidget: ControlWidget {
-    static let kind: String = "ControlWidget"
+    static let kind: String = "com.EasyTier.control-widget"
 
     var body: some ControlWidgetConfiguration {
         ControlWidgetConfiguration {
-            ControlWidgetButton(action: {}) {
-                Label("Control", systemImage: "switch.2")
+            ControlWidgetButton(action: {
+                // 这里放你的点击逻辑
+            }) {
+                Label("EasyTier", systemImage: "network.badge.shield.half.filled")
             }
         }
+        .configurationDisplayName("EasyTier Control")
+        .description("Quick control for EasyTier VPN")
     }
 }
 
-// 给低版本 iOS 提供兼容占位，防止编译报错
-@available(iOS, introduced: 16.0, deprecated: 18.0, message: "ControlWidget only available on iOS 18+")
-struct LegacyControlWidget: Widget {
-    var body: some WidgetConfiguration {
-        StaticConfiguration(kind: "LegacyControl", provider: LegacyProvider()) { entry in
-            Text("Control Widget")
-        }
-        .configurationDisplayName("Control")
-        .description("Control widget for iOS 18+")
-        .supportedFamilies([.systemSmall])
-    }
-}
-
-struct LegacyProvider: TimelineProvider {
-    func placeholder(in context: Context) -> LegacyEntry { LegacyEntry(date: Date()) }
-    func getSnapshot(in context: Context, completion: @escaping (LegacyEntry) -> Void) { completion(LegacyEntry(date: Date())) }
-    func getTimeline(in context: Context, completion: @escaping (Timeline<LegacyEntry>) -> Void) { completion(Timeline(entries: [LegacyEntry(date: Date())], policy: .atEnd)) }
-}
-
-struct LegacyEntry: TimelineEntry { let date: Date() }
-
+// 注意：如果你的 Widget 是在 @main 里注册的，一定要加版本判断：
+// @main
+// struct EasyTierWidgets: WidgetBundle {
+//     var body: some Widget {
+//         if #available(iOS 18.0, *) {
+//             EasyTierControlWidget()
+//         }
+//         // 其他兼容 iOS 16 的 Widget
+//         EasyTierRegularWidget()
+//     }
+// }
 #endif
